e4dbe2446824caf8a8d665f0b28276ba98ed0e52
Tharindu Theekshana <138811687+ThariTheekshana@users.noreply.github.com>
2024-10-13 01:06:56 +0530

    completed

---
+++ TripPlaner/TripPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlanerApp.swift
+//  TripPlaner
+//
+//  Created by Tharindu Theekshana on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct TripPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TripPlanerTests/TripPlanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlanerTests.swift
+//  TripPlanerTests
+//
+//  Created by Tharindu Theekshana on 2024-10-11.
+//
+
+import Testing
+@testable import TripPlaner
+
+struct TripPlanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
